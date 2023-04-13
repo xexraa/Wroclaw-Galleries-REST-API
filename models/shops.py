f5e23cdac9ead6ea57c65d3e566ef9c8ee497a56
adrian <adrian@example.com>
2023-04-13 14:55:13 +0200

    delete line

--- models/shops.py
+++ models/shops.py
@@ -9,33 +9,3 @@ class ShopModel(db.Model):
 
     store = db.relationship('StoreModel', back_populates='shops')
 
-
-    
-    
-    
-    
-    
-    
-# class CafeModel(db.Model):
-#     __tablename__ = "cafes"
-    
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
-#     store = db.relationship('StoreModel', backref='cafes', lazy=True)
-
-# class RestaurantModel(db.Model):
-#     __tablename__ = "restaurants"
-    
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
-#     store = db.relationship('StoreModel', backref='restaurants', lazy=True)
-
-# class ServicePointModel(db.Model):
-#     __tablename__ = "service_points"
-    
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
-#     store = db.relationship('StoreModel', backref='service_points', lazy=True)
